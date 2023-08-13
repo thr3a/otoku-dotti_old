b21e78f448d493894dd21ffecd406eaf5eac9c2e
thr3a <thr3a@users.noreply.github.com>
2023-08-13 13:54:19 +0000

    deploy: c50ef1467db5b237b195964b195e327af106f2b1

--- _next/static/nyQltEOqcmjmRaxBNi9xZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/362-43f1a1fa27ca96a5.js","static/chunks/pages/index-cb0a9ee01c651bbc.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
